e2262b88095db0fac1d6ade7b134e4b752ff8c8e
sutheeshsham7 <42170136+sutheeshsham7@users.noreply.github.com>
2018-08-09 12:45:38 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,19 @@
+ class sum
+{
+    public static void main(String[] args) 
+    {
+        int s=3;
+        if(s> 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(s< 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+    }
+}
